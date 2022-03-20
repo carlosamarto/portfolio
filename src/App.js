ca5fae49d12df99d5cfad3120ba06a40151b0d19
DNC\carlo <carlosmrtzow8@gmail.com>
2022-03-20 00:56:28 -0400

    update path

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Intro from "./components/sections/Intro";
 import Contact from "./components/sections/Contact";
 import Projects from "./components/sections/Projects";
 import Featured from "./components/sections/Featured";
-import Container from "./components/Container/Container";
+import Container from "./components/container/Container";
 
 function App() {
   return (
