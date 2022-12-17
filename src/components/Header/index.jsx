1658e41915d48c315c942b55d28f2719953dac17
Carlos Martinez <cmartinez@mcontigo.com>
2022-12-17 01:00:56 -0400

    Update: Header Structure & Logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,35 @@
 import "./Header.css";
 
 function Header() {
-	return <>Header</>;
+	return (
+		<>
+			<header className="portfolio--header">
+				<img src="./Logo.svg" alt="Carlos Martínez Logo" className="portfolio--header-logo" />
+
+				<ul className="portfolio--header-menu">
+					<li>
+						<a href="#intro">Intro</a>
+					</li>
+
+					<li>
+						<a href="#about">About</a>
+					</li>
+
+					<li>
+						<a href="#featured">Featured</a>
+					</li>
+
+					<li>
+						<a href="#projects">Projects</a>
+					</li>
+
+					<li>
+						<a href="#contact">Contact</a>
+					</li>
+				</ul>
+			</header>
+		</>
+	);
 }
 
 export { Header };
