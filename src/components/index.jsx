c88d4c6e44b3a471c7713aa95d56745bf642f5c2
Carlos Martinez <cmartinez@mcontigo.com>
2022-12-17 01:04:58 -0400

    Fix: Spacings in index components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,12 +1,8 @@
-export { App } from './App/';
-export { Header } from './Header/';
-export { Intro } from './Intro/';
-export { About } from './About/';
-export { Featured } from './Featured/';
-export { Projects } from './Projects/';
-export { Contact } from './Contact/';
-export { Footer } from './Footer/';
-
-
-
-
+export { App } from "./App/";
+export { Header } from "./Header/";
+export { Intro } from "./Intro/";
+export { About } from "./About/";
+export { Featured } from "./Featured/";
+export { Projects } from "./Projects/";
+export { Contact } from "./Contact/";
+export { Footer } from "./Footer/";
