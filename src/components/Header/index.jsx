a8e867ce0f71ce58c9a07f710f201f631e446a2d
Carlos Martinez <cmartinez@mcontigo.com>
2022-12-20 15:44:45 -0400

    Update: Header, Icons & Logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { CgMenuRight } from "react-icons/cg";
 
 function Header() {
 	return (
@@ -6,27 +7,9 @@ function Header() {
 			<header className="portfolio--header">
 				<img src="./Logo.svg" alt="Carlos Martínez Logo" className="portfolio--header-logo" />
 
-				<ul className="portfolio--header-menu">
-					<li>
-						<a href="#intro">Intro</a>
-					</li>
-
-					<li>
-						<a href="#about">About</a>
-					</li>
-
-					<li>
-						<a href="#featured">Featured</a>
-					</li>
-
-					<li>
-						<a href="#projects">Projects</a>
-					</li>
-
-					<li>
-						<a href="#contact">Contact</a>
-					</li>
-				</ul>
+				<span className="portfolio--header-icon">
+					<CgMenuRight />
+				</span>
 			</header>
 		</>
 	);
