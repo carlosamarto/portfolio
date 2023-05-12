36079da517a641095a8ab006ac3dc869fa517f54
Carlos Martinez <cmartinez@mcontigo.com>
2023-05-12 17:51:42 -0300

    Update: Header Structure

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,47 +1,92 @@
 import "./Header.css";
-import { Logo } from "../Logo/Logo";
+import { Logo } from "../../assets/Logo";
 
-function Header({ initialScroll, scrollDirection, openMenu, setOpenMenu }) {
-	console.log(initialScroll);
-
+function Header({ initialScroll, scrollDirection, openMenu, setOpenMenu, isDesktop }) {
 	return (
 		<>
 			<header
-				id={initialScroll ? "shadow" : "header"}
+				id={initialScroll ? "initial" : "header"}
 				className={`header ${scrollDirection === "down" ? "hide" : "show"}`}
 			>
 				<div>
 					<Logo width="48" height="48" />
 				</div>
 
-				<div
-					className={`header--icon ${openMenu && "header--open"}`}
-					onClick={() => setOpenMenu((prevState) => !prevState)}
-				>
-					<div></div>
-				</div>
+				{!isDesktop && (
+					<div
+						className={`header--icon ${openMenu && "header--open"}`}
+						onClick={() => setOpenMenu((prevState) => !prevState)}
+					>
+						<div></div>
+					</div>
+				)}
 
-				<ul className={`header--menu ${openMenu && "header--open"}`}>
-					<li>
-						<a href="#intro">Intro</a>
-					</li>
 
-					<li>
-						<a href="#about">About</a>
-					</li>
+				{isDesktop ? (
+					<ul className={`header--menu ${openMenu && "header--open"}`}>
+						<li>
+							<a href="#intro">
+								<span>01.</span> Intro
+							</a>
+						</li>
 
-					<li>
-						<a href="#featured">Featured</a>
-					</li>
+						<li>
+							<a href="#about">
+								<span>02.</span> About
+							</a>
+						</li>
 
-					<li>
-						<a href="#projects">Projects</a>
-					</li>
+						<li>
+							<a href="#featured">
+								<span>03.</span> Featured
+							</a>
+						</li>
 
-					<li>
-						<a href="#contact">Contact</a>
-					</li>
-				</ul>
+						<li>
+							<a href="#projects">
+								<span>04.</span> Projects
+							</a>
+						</li>
+
+						<li>
+							<a href="#contact">
+								<span>05.</span> Contact
+							</a>
+						</li>
+					</ul>
+				) : (
+					<ul className={`header--menu ${openMenu && "header--open"}`}>
+						<li>
+							<a href="#intro">
+								<span>01.</span> Intro
+							</a>
+						</li>
+
+						<li>
+							<a href="#about">
+								<span>02.</span> About
+							</a>
+						</li>
+
+						<li>
+							<a href="#featured">
+								<span>03.</span> Featured
+							</a>
+						</li>
+
+						<li>
+							<a href="#projects">
+								<span>04.</span> Projects
+							</a>
+						</li>
+
+						<li>
+							<a href="#contact">
+								<span>05.</span> Contact
+							</a>
+						</li>
+					</ul>
+				)}
 			</header>
 		</>
 	);
