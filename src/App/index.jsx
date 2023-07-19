128137111c180e68849cc19ef02b564fe2f96680
Carlos Martinez <cmartinez@mcontigo.com>
2023-07-18 23:11:51 -0300

    Update/WPI: Fix Folder Name

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useAnimationTimer, useChangeLang, useHeaderScroll, useLoadingContent, useOpenMenu } from "../CustomHooks";
+import { useAnimationTimer, useChangeLang, useHeaderScroll, useLoadingContent, useOpenMenu } from "../Hooks";
 import { Context } from "../Context";
 import { Content, Footer, Header, Loader } from "../Layouts";
 import { About, Contact, Intro, Projects } from "../Sections";
