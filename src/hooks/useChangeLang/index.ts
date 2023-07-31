fd601ed09bc043d2cfb4e7ac8a60c8c4a836daf0
Carlos Martinez <cmartinez@mcontigo.com>
2023-07-31 20:14:36 -0300

    update/wpi: Fix bugs and warnings

--- src/hooks/useChangeLang/index.ts
+++ src/hooks/useChangeLang/index.ts
@@ -1,4 +1,3 @@
-// React
 import { useState, useEffect } from 'react'
 
 // Utils
@@ -6,21 +5,25 @@ import { type LangFunction, type LangProps } from '@/utils/definitions'
 
 // Custom Hook
 const useChangeLang = ({ key, initialValue }: LangProps): [string, LangFunction] => {
+  const isClient = typeof window !== 'undefined' // Check if it is running in the browser
+
   const [lang, setLang] = useState(() => {
-    // Get the stored value from localStorage
-    const storedValue = localStorage.getItem(key)
+    // Get the stored value from localStorage if it's available
+    const storedValue = isClient ? localStorage.getItem(key) : null
 
     // Use the stored value or the initial value
     return storedValue !== null ? storedValue : initialValue
   })
 
   useEffect(() => {
-    // Set the lang attribute of the document
-    document.documentElement.lang = lang
+    if (isClient) {
+      // Set the lang attribute of the document
+      document.documentElement.lang = lang
 
-    // Store the language value in localStorage
-    localStorage.setItem(key, lang)
-  }, [key, lang])
+      // Store the language value in localStorage
+      localStorage.setItem(key, lang)
+    }
+  }, [isClient, key, lang])
 
   // Toggle language function
   const toggleLang: LangFunction = (value) => {
