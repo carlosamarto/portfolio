5ef6ea685bb298311db09621b05e1ac9f16e6cd3
Carlos Martinez <cmartinez@mcontigo.com>
2023-08-03 15:45:46 -0300

    update/wpi: fix button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,7 @@
 export default function Button ({ link, external, text }: { link: string, external: boolean, text: string }): JSX.Element {
   return (
     <>
-      <a href={`/${link}`} target={external ? '_blank' : ''} rel={external ? 'noopener noreferrer' : ''} className="button">
+      <a href={`${link}`} target={external ? '_blank' : ''} rel={external ? 'noopener noreferrer' : ''} className="button">
         <span className="button__inner">
           <span className="button__content">{text}</span>
         </span>
